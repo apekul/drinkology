817233563646ef02bb60f41e3e46c7a081774912
Adaś <adampekul@gmail.com>
2024-01-16 00:10:45 +0100

    fix random drink error Cannot read properties of null

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -307,15 +307,14 @@ const Search = () => {
           ))}
         </ul>
         {/* random drink */}
-        {randomDrink && (
-          <div
-            className={`flex flex-col gap-5 border-b-2 pb-5`}
-            id="randomDrink"
-          >
-            <p className="font-bold">Random Drink</p>
-            <DisplayItem item={randomDrink} bg="bg-indigo-100" />
-          </div>
-        )}
+        <div id="randomDrink">
+          {randomDrink && (
+            <div className={`flex flex-col gap-5 border-b-2 pb-5`}>
+              <p className="font-bold">Random Drink</p>
+              <DisplayItem item={randomDrink} bg="bg-indigo-100" />
+            </div>
+          )}
+        </div>
         {/* Result */}
         <ul className="flex flex-col gap-5" id="drinkResults">
           <p className="font-bold">
